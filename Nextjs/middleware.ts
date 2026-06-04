ddf1b81b59ad0051bc45226ae901105aaa480368
leith-allah <leithallah.contact@gmail.com>
2026-06-04 21:51:57 +0100

    AUTH V1.3 ✓ JWT ✓ Login ✓ Logout ✓ Cookie HttpOnly ✓ /api/auth/me ✓ Middleware actif ✓ Protection dashboard ✓ Build OK

--- Nextjs/middleware.ts
+++ Nextjs/middleware.ts
@@ -1,15 +1,89 @@
 
+import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  const isLoggedIn = true;
+import { verifyToken } from "@/server/auth/jwt";
 
-  if (!isLoggedIn) {
+import { AUTH_COOKIE_NAME } from "@/server/constants/auth.constants";
+
+export function middleware(
+  request: NextRequest
+) {
+
+  const token =
+    request.cookies.get(
+      AUTH_COOKIE_NAME
+    )?.value;
+
+  const pathname =
+    request.nextUrl.pathname;
+
+  // Routes publiques
+  const publicRoutes = [
+    "/",
+    "/login",
+    "/register",
+    "/about-us",
+    "/contact",
+    "/unauthorized",
+    "/forbidden",
+  ];
+
+  if (
+    publicRoutes.includes(
+      pathname
+    )
+  ) {
+    return NextResponse.next();
+  }
+
+  if (!token) {
+
     return NextResponse.redirect(
-      new URL("/unauthorized", request.url)
+      new URL(
+        "/unauthorized",
+        request.url
+      )
     );
   }
 
-  return NextResponse.next();
+  try {
+
+    const user =
+      verifyToken(token);
+
+    // ADMIN ONLY
+    if (
+      pathname.startsWith(
+        "/dashboard/packages/create"
+      ) &&
+      user.role !== "ADMIN"
+    ) {
+
+      return NextResponse.redirect(
+        new URL(
+          "/forbidden",
+          request.url
+        )
+      );
+    }
+
+    return NextResponse.next();
+
+  } catch {
+
+    return NextResponse.redirect(
+      new URL(
+        "/unauthorized",
+        request.url
+      )
+    );
+  }
 }
+
+export const config = {
+
+  matcher: [
+    "/dashboard/:path*",
+  ],
+};
